9628e276e5e837f222ad1a8a1722c5294b0265d5
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-02-10 21:03:47 -0300

    criando o repositorio de cliente

--- Fiap.Web.AspNet2/Repository/ClienteRepository.cs
+++ Fiap.Web.AspNet2/Repository/ClienteRepository.cs
@@ -1,11 +1,54 @@
-﻿using System;
+﻿using Fiap.Web.AspNet2.Data;
+using Fiap.Web.AspNet2.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Fiap.Web.AspNet2.Repository
 {
     public class ClienteRepository
     {
+        public DataContext _context { get; set; }
+
+        public ClienteRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        public List<ClienteModel> FindAll()
+        {
+            return _context.Clientes.ToList<ClienteModel>(); ;
+        }
+
+        public ClienteModel FindById(int id)
+        {
+            return _context.Clientes.Find(id);
+        }
+
+        public void Insert(ClienteModel ClienteModel)
+        {
+            _context.Clientes.Add(ClienteModel);
+            _context.SaveChanges();
+        }
+
+        public void Update(ClienteModel ClienteModel)
+        {
+            _context.Clientes.Update(ClienteModel);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ClienteModel ClienteModel = new ClienteModel(id, "", "");
+            Delete(ClienteModel);
+        }
+
+
+        public void Delete(ClienteModel ClienteModel)
+        {
+            _context.Clientes.Remove(ClienteModel);
+            _context.SaveChanges();
+        }
+
     }
 }
